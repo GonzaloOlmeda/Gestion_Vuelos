6097783ac81055ae0fd205b439a88f1dcb0b4d40
Gonzalo Olmeda <gonzalo.olmeda06@gmail.com>
2025-12-14 10:53:57 +0100

    primer commit

---
+++ src/main/java/es/daw/gestion_vuelos_apirest/GestionVuelosApiRestApplication.java
@@ -0,0 +1,13 @@
+package es.daw.gestion_vuelos_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVuelosApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVuelosApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/daw/gestion_vuelos_apirest/GestionVuelosApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.daw.gestion_vuelos_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVuelosApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
